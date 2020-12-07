ce0f4e4e2b3df5ffd50cf4729878bb2f259fc40b
dohyung1 <dohyung1@alumni.cmu.edu>
2020-12-07 00:54:46 -0500

    Fourth commit

---
+++ instagram/Resources/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  instagram
+//
+//  Created by Administrator on 12/6/20.
+//
+
+import UIKit
+
+extension UIView{
+    
+    public var width:CGFloat{
+        return frame.size.width
+    }
+    
+    public var height:CGFloat{
+        return frame.size.height
+    }
+    
+    public var top:CGFloat{
+        return frame.origin.y
+    }
+    
+    public var bottom:CGFloat{
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left:CGFloat{
+        return frame.origin.x
+    }
+    
+    public var right:CGFloat{
+        return frame.origin.x + frame.size.width
+    }
+}
